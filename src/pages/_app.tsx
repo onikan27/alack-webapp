9f223fee822a6c0a8104ae67cad3e5b4562b139d
onion27 <ude0721juji@icloud.com>
2022-08-20 00:40:33 +0900

    feat: add chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import { client } from 'src/lib/apolloClient'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ApolloProvider>
   )
 }
